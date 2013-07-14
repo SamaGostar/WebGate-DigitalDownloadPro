0515c5e53834db86dcd9e676d66aea9b188a700b
MA7 <amini141@gmail.com>
2013-07-14 17:10:38 +0430

    initial

---
+++ form.tpl.php
@@ -0,0 +1,33 @@
+﻿<?php
+	// ZarinPal Gateway by M.Amini
+	if (!defined("_VALID_PHP")) 
+		die("دسترسی مستقیم امکانپذیر نیست");
+	$cartrow = $content->getCartContent();
+	$totalrow = $content->getCartTotal();
+	$amount = $totalrow["total"] - $totalrow["coupon"];
+	$amount = $amount * 1000;
+	$merchantID = $row["extra"];
+	$desc = $row["extra2"];
+	$callback = SITEURL."/gateways/".$row["dir"]."/ipn.php";
+	$client = new SoapClient("https://www.zarinpal.com/pg/services/WebGate/wsdl", array("encoding"=>"UTF-8"));
+	$payID = $client->PaymentRequest(
+			array(
+							'MerchantID' 	=> $merchantID ,
+							'Amount' 		=> $amount ,
+							'Description' 	=> $desc ,
+							'Email' 		=> '' ,
+							'Mobile' 		=> '' ,
+							'CallbackURL' 	=> $callback
+
+							)
+			 );
+	if ($payID->Status != 100) {
+		die("در اتصال به درگاه زرین پال، مشکلی پیش آمده است");
+	}
+	$_SESSION["zarin_amount"] = $amount;
+	$_SESSION["zarin_1"] = $user->uid."_".$user->sesid;
+	$_SESSION["zarin_2"] = $user->email;
+?>
+<form action="https://www.zarinpal.com/pg/StartPay/<?php echo $payID->Authority; ?>" method="POST" name="zarin">
+	<input type="image" src="<?php echo SITEURL."/gateways/".$row["dir"]."/ZarinPal_big.png"?>" class="tooltip" title="پرداخت آنلاین با زرین پال" onclick="document.zarin.submit()" />
+</form>
---
+++ ipn.php
@@ -0,0 +1,61 @@
+﻿<?php
+	// ZarinPal Gateway by M.Amini
+	session_start();
+	define("_VALID_PHP",true);
+	require_once("../../init.php");
+	$merchantID = getValue("extra", "gateways", "name = 'زرین پال'");
+	$amount = $_SESSION["zarin_amount"];
+	$au = $_GET["Authority"];
+	$client = new SoapClient("https://www.zarinpal.com/pg/services/WebGate/wsdl", array("encoding"=>"UTF-8"));
+	$statusID =PaymentVerification(
+			array(
+					'MerchantID'	 => $merchantID ,
+					'Authority' 	 => $au ,
+					'Amount'	 	=> $amount
+				)
+		);
+	
+	
+	if ($statusID->Status == 100) {
+		list($user_id, $sesid) = explode("_", $_SESSION["zarin_1"]);
+		
+		$cartrow = $content->getCartContent($sesid);
+		$totalrow = $content->getCartTotal($sesid);
+		$gross = $totalrow["total"] - $totalrow["coupon"];
+		
+		$payer_email = $_SESSION["zarin_2"];
+		$receiver_email = getValue("extra3", "gateways", "name = 'ZarinPal'");
+		$currency = "toman";
+		$txn_id = $au;
+		
+		if ($cartrow) {
+			foreach ($cartrow as $crow) {
+			$data = array(
+				"txn_id" => sanitize($txn_id),
+				"pid" => $crow["pid"],
+				"uid" => intval($user_id),
+				"downloads" => 0,
+				"file_date" => time(),
+				"ip" => sanitize($_SERVER["REMOTE_ADDR"]),
+				"created" => "NOW()",
+				"payer_email" => $payer_email,
+				"payer_status" => "verified",
+				"item_qty" => $crow["total"],
+				"price" => $crow["total"] * $crow["price"] * 1000,
+				"currency" => sanitize($currency),
+				"pp" => "ZarinPal",
+				"status" => 1,
+				"active" => 1
+			);
+			$db->insert("transactions", $data);
+		}
+	}
+	header("Location: ".SITEURL."/account.php");
+	} else {
+		echo("<p style='direction:rtl;font:10pt Tahoma'>در هنگام پرداخت مشکلی به وجود آمد. با مدیر تماس بگیرید</p>");
+	}
+	$_SESSION["zarin_amount"] = "";
+	$_SESSION["zarin_1"] = "";
+	$_SESSION["zarin_2"] = "";
+	die();
+?>
